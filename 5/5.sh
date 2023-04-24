89762753cfaba01f4f9669e529e426eebc8aa96d
Aleko Georgiev <61247544+unamusedchemical@users.noreply.github.com>
2023-04-24 17:27:21 +0300

    Update 5.sh

--- 5/5.sh
+++ 5/5.sh
@@ -4,6 +4,7 @@ read -p "Enter name of file" filename
 read -p "Enter pattern" pattern
 
 count=$( grep -E "$pattern" "$filename" | wc -l )
+exit=$?
 
 if [[ count -gt 0 ]]; then
 	echo "Sequence is present in file"
@@ -11,4 +12,4 @@ else
 	echo "Sequence is not present in file"
 fi
 
-echo "Exit code is $?"
+echo "Exit code is $exit"
